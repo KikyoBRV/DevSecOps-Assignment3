1a8179d8e0309de4dd5bb08b0b4a956b27e31cc9
Buravit Yenjit <115054272+KikyoBRV@users.noreply.github.com>
2026-01-11 22:57:01 +0700

    Initial commit

---
+++ example-5/video-streaming/src/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const fs = require("fs");
+const amqp = require('amqplib');
+
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+
+if (!process.env.RABBIT) {
+    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
+}
+
+const PORT = process.env.PORT;
+const RABBIT = process.env.RABBIT;
+
+//
+// Application entry point.
+//
+async function main() {
+	
+    console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
+
+    const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
+    
+    console.log("Connected to RabbitMQ.");
+
+    const messageChannel = await messagingConnection.createChannel(); // Creates a RabbitMQ messaging channel.
+
+	await messageChannel.assertExchange("viewed", "fanout"); // Asserts that we have a "viewed" exchange.
+
+    //
+    // Broadcasts the "viewed" message to other microservices.
+    //
+    function broadcastViewedMessage(messageChannel, videoId, videoPath) {
+        console.log(`Publishing message on "viewed" exchange.`);
+
+        const msg = { videoId: videoId, videoPath: videoPath };
+        const jsonMsg = JSON.stringify(msg);
+        messageChannel.publish("viewed", "", Buffer.from(jsonMsg));
+    }
+
+
+    const app = express();
+
+    app.get("/video", async (req, res) => {
+
+        const videoId = parseInt(req.query.id, 10);
+
+        if (![1, 2].includes(videoId)) {
+            return res.status(400).send("Please provide id=1 or id=2");
+        }
+
+        const videoPath = videoId === 1
+            ? "./videos/video-1.mp4"
+            : "./videos/video-2.mp4";
+
+        const stats = await fs.promises.stat(videoPath);
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+
+        fs.createReadStream(videoPath).pipe(res);
+
+        broadcastViewedMessage(messageChannel, videoId, videoPath);
+    });
+
+    app.listen(PORT, () => {
+        console.log("Microservice online.");
+    });
+}
+
+main()
+    .catch(err => {
+        console.error("Microservice failed to start.");
+        console.error(err && err.stack || err);
+    });
